59855c7c25335a1387be0db7443127ddb44a2f5b
Daniel Ho <dho1994@gmail.com>
2021-10-13 10:41:35 -0700

    Added effects and styling to main geometry

--- client/src/components/EdgeShape.jsx
+++ client/src/components/EdgeShape.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom'
 import React, { useRef, useState, useEffect } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
+import * as THREE from 'three';
 
 function EdgeShape(props) {
   // This reference will give us direct access to the THREE.Mesh object
@@ -8,11 +9,13 @@ function EdgeShape(props) {
   // Set up state for the hovered and active state
   const [hovered, setHover] = useState(false)
   const [active, setActive] = useState(false)
+
+  const color = new THREE.Color()
   // Subscribe this component to the render-loop, rotate the mesh every frame
   useFrame((state, delta) => {
-    ref.current.rotation.x += hovered ? 0.01 : 0.01;
-    ref.current.rotation.y += hovered ? 0.01 : 0.01;
-    ref.current.rotation.z += hovered ? 0.01 : 0.01;
+    ref.current.rotation.x += active ? -0.01 : 0.01;
+    ref.current.rotation.y += active ? -0.01 : 0.01;
+    ref.current.rotation.z += active ? -0.01 : 0.01;
     // const b = document.body.getBoundingClientRect().bottom;
     // ref.current.position.z = b * 0.01
 
@@ -21,17 +24,19 @@ function EdgeShape(props) {
     ref.current.scale.x = Math.max(1, -b * 0.01);
     ref.current.scale.y = Math.max(1, -b * 0.01);
     ref.current.scale.z = Math.max(1, -b * 0.01);
+
+    ref.current.material.color.lerp(color.set(hovered ? "darkTurquoise" : "white").convertSRGBToLinear(), hovered ? 0.1 : 0.05)
   })
 
-  // useEffect(() => {
-  //   document.body.style.cursor = hovered ? 'pointer' : 'auto'
-  // }, [hovered])
+  useEffect(() => {
+    document.body.style.cursor = hovered ? 'pointer' : 'auto'
+  }, [hovered])
 
   return (
     <mesh
       // {...props}
       ref={ref}
-      scale={active ? 1 : 1}
+      scale={1}
       onClick={(event) => setActive(!active)}
       onPointerOver={(event) => setHover(true)}
       onPointerOut={(event) => setHover(false)}
